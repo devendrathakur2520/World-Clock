378532af1a8d0c519d661416d721f1f02470dc1f
KAPIL THAKUR <KAPPU445@GMAIL.com>
2023-11-01 15:17:17 +0530

    world clock

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Countries from "./components/Countries";
 import Navbar from "./components/Navbar";
+// import StopWatch from "./components/Stopwatch";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Countries />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/clock" element={<Countries />} />
+        </Routes>
+        {/* <Routes>
+          <Route path="/stopwatch" element={<StopWatch />} />
+        </Routes> */}
+        {/* <Countries /> */}
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,21 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 function CollapsibleExample() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+    <Navbar collapseOnSelect expand="lg" className="bg-body-secondary">
       <Container>
-        <Navbar.Brand href="#home">NavBar</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className=" mx-5">
-            <Nav.Link to="/clock">Clock</Nav.Link>
-            <Nav.Link href="#stopwatch">StopWatch</Nav.Link>
+          <Nav className=" mx-2">
+            <Link className="mx-5" to="/clock">
+              Clock
+            </Link>
+            <Link className="mx-5" to="/stopwatch">
+              StopWatch
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
